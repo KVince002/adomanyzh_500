95a5f4e7c2ee168612b374602c22858b65183ddd
Kemény Vince <foxagent002@gmail.com>
2022-02-26 19:14:03 +0100

    Felkész főoldal

---
+++ assets/js/foOldaljs.js
@@ -0,0 +1,20 @@
+Betoltes();
+function Betoltes() {
+    const formData = new FormData();
+    formData.append("funkcio", "Betoltes");
+    fetch(baseUrl + "/ajax/FoOldalPHP.php", {
+        method: "POST",
+        body: formData,
+    })
+        .then(Response => Response.text())//gyanús, hogy soem dobja be hogy text()
+        .then(Request => {
+            console.log(Request);
+            var betoltes = JSON.parse(Request);
+            //generáló funkcio
+            kartyaGeneral(betoltes);
+        })
+}
+
+function kartyaGeneral(btTomb) {
+
+}
