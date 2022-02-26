965d6c318f27bfa532b28404cf3c4af405855566
Kemény Vince <foxagent002@gmail.com>
2022-02-26 19:14:26 +0100

    Felkész felhasználó

--- ajax/FelhasznaloLoginPHP.php
+++ ajax/FelhasznaloLoginPHP.php
@@ -4,7 +4,7 @@ require_once "../app/functions.php";
 if (isset($_POST["funkcio"])) {
     if ($_POST["Bejelentkez"]) {
         Bejelentkez($_POST["email"], $_POST["jelsz"], $conn);
-    } elseif ($_POST["FelhaszRegisztral"]) {
-        FelhaszRegisztral($_POST["vnev"], $_POST["knev"], $_POST["bnev"], $_POST["email"], $_POST["telsz"], $_POST["jelsz"], $conn);
+    } elseif ($_POST["FRegisztral"]) {
+        FRegisztral($_POST["vnev"], $_POST["knev"], $_POST["bnev"], $_POST["email"], $_POST["telsz"], $_POST["jelsz"], $conn);
     }
 }
