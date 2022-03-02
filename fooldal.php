3c4e5708f1488a7bb1fadd7bacf505ec07a2a34a
Kemény Vince <foxagent002@gmail.com>
2022-03-02 19:19:11 +0100

    mint az előbi

---
+++ fooldal.php
@@ -0,0 +1,28 @@
+<?php
+require_once "app/functions.php";
+Kijelentkezes();
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<?php
+require_once "common/head2.php"
+?>
+
+<body>
+    <?php
+    require_once "common/public_nav.php";
+    ?>
+    <main class="mdl-layout__content">
+        <div class="page-content">
+            <div id="Generalo">
+
+            </div>
+        </div>
+    </main>
+    <?php
+    require_once "common/public_footer.php";
+    ?>
+    <script src="<?= BASEURL ?>/assets/js/foOldaljs.js"></script>
+</body>
+
+</html>
