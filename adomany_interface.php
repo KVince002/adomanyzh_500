722c9a0f306b217e817a04b32504460c5fc006a6
Vincent <93611221+KitsuneRanger@users.noreply.github.com>
2022-03-03 13:31:03 +0100

    Az interface beállítások kártya

--- adomany_interface.php
+++ adomany_interface.php
@@ -9,14 +9,25 @@ require_once "common/head1.php";
 
 <body>
     <?php
-        require_once "common/public_nav.php";
-        JogosultsagEllenorzes(false);
+    require_once "common/public_nav.php";
+    JogosultsagEllenorzes(false);
     ?>
+    <!-- beállítások kártya -->
     <main class="mdl-layout__content">
         <div class="page-content">
             <div class="mdl-grid">
                 <div class="mdl-card mdl-cell mdl-cell--8-col">
-
+                    <div class="mdl-card__title-text">
+                        <h3>Beállítások</h3>
+                    </div>
+                    <div class="mdl-card__supporting-text">
+                        <p>Egy adat átírásához csak kattintson a szövegmezőre és írja bele az új értéket, Majd kattitnson az elfogadás gombra!</p>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input type="text" name="UjMail" id="UjMail" class="mdl-textfiled__input">
+                            <label for="UjMail" id="UjMailLABEL" class="mdl-textfiled__label">Valami</label>
+                        </div>
+                        <button type="button" id="MailFrissitGomb" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Megerősítés</button>
+                    </div>
                 </div>
             </div>
         </div>
