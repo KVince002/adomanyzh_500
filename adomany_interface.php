a45344f36f92edc70c624c6ee3cf907043be08d3
Vincent <93611221+KitsuneRanger@users.noreply.github.com>
2022-03-03 18:04:47 +0100

    Adomány interface frissítés
    A beállítások kártya már megfelelően megjelenik.

--- adomany_interface.php
+++ adomany_interface.php
@@ -17,16 +17,47 @@ require_once "common/head1.php";
         <div class="page-content">
             <div class="mdl-grid">
                 <div class="mdl-card mdl-cell mdl-cell--8-col">
-                    <div class="mdl-card__title-text">
-                        <h3>Beállítások</h3>
+                    <div class="mdl-card__title">
+                        <h2 class="mdl-card__title-text">Beállítások</h2>
                     </div>
                     <div class="mdl-card__supporting-text">
-                        <p>Egy adat átírásához csak kattintson a szövegmezőre és írja bele az új értéket, Majd kattitnson az elfogadás gombra!</p>
+                        <p>Egy adat átírásához csak kattintson a szövegmezőre és írja bele az új értéket, Majd kattitnson az <b>Megerősítés</b> gombra!</p>
                         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                            <input type="text" name="UjMail" id="UjMail" class="mdl-textfiled__input">
-                            <label for="UjMail" id="UjMailLABEL" class="mdl-textfiled__label">Valami</label>
+                            <label class="mdl-textfield__label" for="UjMail">[jelenlegi email]</label>
+                            <input class="mdl-textfield__input" type="email" name="Ujmail" id="UjMaill" autofocus required autocomplete="email">
+                            <br>
                         </div>
                         <button type="button" id="MailFrissitGomb" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Megerősítés</button>
+
+                        <!-- új Jelszó -->
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <label class="mdl-textfield__label" for="UjJelszo">[jelenlegi Jelszo]</label>
+                            <input class="mdl-textfield__input" type="text" name="UjJelszo" id="UjJelszo" required>
+                            <br>
+                        </div>
+                        <button type="button" id="JelszoFrissitGomb" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Megerősítés</button>
+
+                        <!-- név változtatás -->
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <label class="mdl-textfield__label" for="UjCim">[Jelenegi cím]</label>
+                            <input class="mdl-textfield__input" type="text" name="UjCim" id="UjCim" required maxlength="249">
+                            <br>
+                        </div>
+                        <button type="button" id="CimFrissitGomb" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Megerősítés</button>
+
+                        <!-- leírás változtatás -->
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <label class="mdl-textfield__label" for="UjLe">Leírás</label>
+                            <input class="mdl-textfield__input" type="text" name="UjLe" id="UjLe" required maxlength="249">
+                            <br>
+                        </div>
+                        <button type="button" id="LeFrissitGomb" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Megerősítés</button>
+
+                        <!-- fiók törlése -->
+                        <p>Biztos törölni szeretné a szervezet profilját?</p>
+                        <input type="checkbox" name="Igen" id="Igen">
+                        <br>
+                        <button type="button" name="Torol" id="Torol" disabled="true"><b>Törlés</b></button>
                     </div>
                 </div>
             </div>
